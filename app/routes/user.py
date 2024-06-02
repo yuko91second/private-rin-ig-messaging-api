b8de1663a94e23a82a4e0de1535f05fa3123e712
noriharu888 <newyork.soccer0824@gmail.com>
2024-06-02 15:49:00 +0900

    test: webhookで受け取るイベント「comments」「messages」ごとに処理を分けてみた

--- app/routes/user.py
+++ app/routes/user.py
@@ -49,17 +49,21 @@ async def post_webhook(body: InstagramMessage):
         # unix_time = entry_obj['time']
         changes_obg = entry_obj['changes'][0]
         change_field = changes_obg['field']
-        change_value_obj = changes_obg['value']
-        sender_obj = change_value_obj['from']
-        media_obj = change_value_obj['media']
-        sender_id = sender_obj['id']
-        sender_username = sender_obj['username']
-        media_id = media_obj['id']
-        media_type = media_obj['media_product_type']
-        text = change_value_obj['text']
-        print(f'> {sender_username}から{media_type}(投稿id: {media_id})に「{text}」という{change_field}メッセージが送られました。')
-        response = "これは応答です"
-        sendCustomerAMessage(FACEBOOK_PAGE_ID, response, FACEBOOK_PAGE_ACCESS_TOKEN, sender_id)
+        if change_field == 'comments':
+            change_value_obj = changes_obg['value']
+            sender_obj = change_value_obj['from']
+            media_obj = change_value_obj['media']
+            sender_id = sender_obj['id']
+            sender_username = sender_obj['username']
+            media_id = media_obj['id']
+            media_type = media_obj['media_product_type']
+            text = change_value_obj['text']
+            print(f'> {sender_username}から{media_type}(投稿id: {media_id})に「{text}」という{change_field}メッセージが送られました。')
+            response = "これは応答です"
+            sendCustomerAMessage(FACEBOOK_PAGE_ID, response, FACEBOOK_PAGE_ACCESS_TOKEN, sender_id)
+        if change_field == 'messages':
+            return {'message': 'Received message.'}
+
         return Response(content='EVENT_RECEIVED', status_code=status.HTTP_200_OK)
     else:
         return Response(status_code=status.HTTP_404_NOT_FOUND)
